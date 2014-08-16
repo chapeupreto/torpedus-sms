2be69487bbaa4497f9dae0c5bcd5ce780e938688
chapeupreto <falajhou@gmail.com>
2014-08-16 20:59:01 -0300

    API em PHP para envio de mensagens SMS usando a solucao da empresa Torpedus (www.torpedus.com.br)

---
+++ README.md
@@ -0,0 +1,52 @@
+# Torpedus SMS
+
+Esse projeto consiste em uma API simples desenvolvida em PHP que pode ser usada para enviar mensagens _SMS_ utilizando o serviço fornecido pela empresa **Torpedus** (www.torpedus.com.br)
+
+[Torpedus](http://www.torpedus.com.br)
+
+# Requisitos
+
+- PHP 5+
+- Biblioteca cURL
+
+# Utilização
+
+Primeiramente, depois de baixar o arquivo `TorpedusSMS.php`, basta incluir o mesmo na sua aplicação:
+
+```php
+include 'TorpedusSMS.php';
+```
+
+Feito isso, basta _instanciar_ a classe usando o nome de usuário e a senha da sua conta cadastrada na _Torpedus_:
+
+```php
+$username = '9083'; // ID da conta cadastrada no site da Torpedus
+$password = 'torp3dus'; // senha cadastrada no site da Torpedus
+
+$torpedus = new TorpedusSMS($username, $password);
+```
+
+Por fim, basta executar o método `send_sms()` do objeto instanciado passando como argumentos o número do telefone do destinatário e a mensagem desejada:
+
+```php
+// se a conta cadastrada na Torpedus tiver crédito, então a mensagem será enviada ao celular do destinatário
+$torpedus->send_sms($destinatario, $mensagem); 
+```
+
+Para tratamento de erros/exceções, pode-se fazer essa chamada dentro de um bloco `try/catch` conforme mostrado a seguir:
+
+```php
+try {
+    $torpedus->send_sms($destinatario, $mensagem);
+} catch(Exception $e) { 
+    echo $e->getMessage();
+}
+```
+
+# Observações
+
+- O número do celular do destinatário deve obedecer ao seguinte formato: _55DDDTelefone_
+
+**Exemplo:** `556481129090`
+
+- Não é necessário remover a acentuação da mensagem a ser disparada via SMS. A API já realiza esse tratamento! :)
---
+++ TorpedusSMS.php
@@ -0,0 +1,148 @@
+<?php
+
+/*
+ * classe de envio de SMS usando a solucao da Torpedus (www.torpedus.com.br)
+ *
+ * Desenvolvido por rod@wgo.com.br
+ * 15/08/2014
+ * WGO Telecom
+ *
+ */
+
+class TorpedusSMS {
+
+	/**
+	 * Usuario da conta torpedus
+	 *
+	 * @var string
+	 */
+
+	protected $username;
+
+	/**
+	 * Senha da conta torpedus
+	 *
+	 * @var string
+	 */
+
+	protected $password;
+
+	/**
+	 * Endereco/URL principal de acesso ao webservice da torpedus
+	 *
+	 * @var string
+	 */
+
+	protected $endereco;
+
+	/**
+	 * Endereco/URL final com os parametros GET para disparar o SMS
+	 *
+	 * @var string
+	 */
+
+	protected $url = '';
+
+	/**
+	 * curl handler para a requisicao
+	 *
+	 * @var resource
+	 */
+
+	protected $curl = null;
+
+	/**
+	 * tamanho maximo para a mensagem a ser enviada via SMS
+	 *
+	 * @var int
+	 */
+
+	const TAMANHO_MAXIMO_MENSAGEM = 304;
+
+	public function __construct($username, $password, $endereco = 'http://torpedus.com.br/sms/index.php?') {
+		$this->username = $username;
+		$this->password = $password;
+		$this->endereco = $endereco;
+
+		$this->curl = curl_init();
+	}
+
+	/**
+	 * envia uma mensagem via SMS ao telefone celular do destinatario
+	 * @param  string $destinatario telefone do destinatario
+	 * @param  string $mensagem		mensagem a ser enviada
+	 * @throws Exception dispara excecao caso a mensagem a ser enviada possua tamanho superior a TAMANHO_MAXIMO_MENSAGEM
+	 * @return  bool              true caso a mensagem seja disparada
+	 */
+	public function send_sms($destinatario, $mensagem) {
+		if(strlen($mensagem) > self::TAMANHO_MAXIMO_MENSAGEM) {
+			throw new Exception('Tamanho da mensagem nao pode ultrapassar '.self::TAMANHO_MAXIMO_MENSAGEM.' caracteres.');
+		}
+
+		return $this->exec($destinatario, $mensagem);
+	}
+
+	private function buildUrl($destinatario, $mensagem) {
+		$query = array(
+					'app' => 'webservices',
+					'u' => $this->username,
+					'p' => $this->password,
+					'ta' => 'pv',
+					'to' => $destinatario,
+					'msg' => $mensagem,
+				);
+
+		$this->url = $this->endereco.http_build_query($query);
+	}
+
+	private function buildCurlOptions() {
+		$options = array(
+					CURLOPT_URL => $this->url,
+					CURLOPT_RETURNTRANSFER => true,
+				);
+		
+		return $options;
+	}
+	
+	private function exec($destinatario, $mensagem) {
+		$this->buildUrl($destinatario, $mensagem);
+		$options = $this->buildCurlOptions();
+		
+		curl_setopt_array($this->curl, $options);
+		$response = curl_exec($this->curl);
+
+		// verifica se houve erro ao executar curl
+		if($errno = curl_errno($this->curl)) {
+			throw new Exception(sprintf('curl error %d: %s', $errno, curl_error($this->curl)));
+		}
+		
+		// execucao curl ok, verifica se a requisicao http tambem retorna OK
+		if(($httpCode = curl_getinfo($this->curl, CURLINFO_HTTP_CODE)) != '200') {
+			throw new Exception(sprintf('Response code of requested resource is %d and it *must* be 200', $httpCode));
+		}
+		
+		return true;
+	}
+
+	public function __destruct() {
+		curl_close($this->curl);
+	}	
+
+} // TorpedusSMS
+
+// Exemplo de utilizacao da classe
+
+$username = '9013'; // ID da conta cadastrada no site da Torpedus
+$password = 'torp3dus'; // senha cadastrada no site da Torpedus
+
+$destinatario = '556481251169'; // numero do celular deve incluir o prefixo 55 e o DDD
+$mensagem = 'Um simples texto pode se transformar em um inferno! Sabe de nada inocente! Que horas são? Açúcar agora!'; // a mensagem deve possuir tamanho maximo de 304 caracteres e pode ou nao ter acentuação
+
+$torpedus = new TorpedusSMS($username, $password);
+
+try {
+	$torpedus->send_sms($destinatario, $mensagem); // se a conta cadastrada na Torpedus tiver crédito, então a mensagem será enviada ao celular do destinatário
+} catch(Exception $e) {	
+	echo $e->getMessage();
+}
+
